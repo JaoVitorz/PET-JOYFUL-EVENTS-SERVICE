c5eff76ad112b516587c58648cc4fc04717444f2
João Vitor J. <jv.jvsantos45@gmail.com>
2025-11-24 19:48:24 -0300

    alterado url swagger pra produção

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -14,7 +14,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3002',
+        url: 'https://pet-joyful-events-service.onrender.com',
         description: 'Servidor de Desenvolvimento'
       }
     ],
